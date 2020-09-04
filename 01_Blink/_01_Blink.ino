149abffda328fa338468fa2617c099153bc56b75
Sarin CR <sarincrg@gmail.com>
2020-09-04 08:02:31 +0530

    Blink LED

---
+++ 01_Blink/_01_Blink.ino
@@ -0,0 +1,25 @@
+struct RGB {
+	int red;
+	int green;
+	int blue;
+};
+
+
+
+
+
+
+
+void setup() {
+	pinMode(13, OUTPUT);
+
+}
+
+
+void loop() {
+  digitalWrite(13, HIGH);
+  delay(1000);
+  digitalWrite(13, LOW);
+  delay(1000);
+
+}
